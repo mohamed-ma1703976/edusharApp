d83ac50b37a080e55ed0fe3b85407c65805ed767
mohamedhossam23 <88920277+mohamed-ma1703976@users.noreply.github.com>
2023-05-24 08:16:09 +0300

    Update Home.dart

--- lib/Home.dart
+++ lib/Home.dart
@@ -6,6 +6,9 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 import 'Provider/DataCacheProvider.dart';
+import 'CoursePage.dart';
+import 'BlogPage.dart';
+import 'EventPage.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -47,13 +50,16 @@ class _HomeState extends State<Home> {
 
               StreamBuilder<QuerySnapshot>(
                 stream: _coursesStream,
-                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
                   if (snapshot.hasError) {
                     return const Text('Something went wrong');
                   }
 
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
+                  if (snapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return const Center(
+                        child: CircularProgressIndicator());
                   }
 
                   return Container(
@@ -63,42 +69,55 @@ class _HomeState extends State<Home> {
                       itemCount: snapshot.data!.docs.length,
                       itemBuilder: (context, index) {
                         final item = snapshot.data!.docs[index];
-                        final data = item.data() as Map<String, dynamic>;
+                        final data =
+                        item.data() as Map<String, dynamic>;
 
-                        return Container(
-                          width: 150,
-                          child: Card(
-                            child: Column(
-                              children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  child: CachedNetworkImage(
-                                    imageUrl: data['fileUrl'],
-                                    placeholder: (context, url) =>
-                                    const CircularProgressIndicator(),
-                                    errorWidget: (context, url, error) =>
-                                    const Icon(Icons.error),
-                                    width: double.infinity,
-                                    height: 100,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                ListTile(
-                                  title: Text(
-                                    data['CourseTitle'],
-                                    style: GoogleFonts.openSans(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    CoursePage(courseId: item.id),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            width: 150,
+                            child: Card(
+                              child: Column(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius:
+                                    BorderRadius.circular(8.0),
+                                    child: CachedNetworkImage(
+                                      imageUrl: data['fileUrl'],
+                                      placeholder: (context, url) =>
+                                      const CircularProgressIndicator(),
+                                      errorWidget: (context, url, error) =>
+                                      const Icon(Icons.error),
+                                      width: double.infinity,
+                                      height: 100,
+                                      fit: BoxFit.cover,
                                     ),
                                   ),
-                                  subtitle: Text(
-                                    'Instructor: ${data['InstructorName']}',
-                                    style: GoogleFonts.openSans(
-                                      fontSize: 12,
+                                  ListTile(
+                                    title: Text(
+                                      data['CourseTitle'],
+                                      style: GoogleFonts.openSans(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      'Instructor: ${data['InstructorName']}',
+                                      style: GoogleFonts.openSans(
+                                        fontSize: 12,
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ),
                         );
@@ -122,13 +141,16 @@ class _HomeState extends State<Home> {
 
               StreamBuilder<QuerySnapshot>(
                 stream: _blogsStream,
-                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
                   if (snapshot.hasError) {
                     return const Text('Something went wrong');
                   }
 
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
+                  if (snapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return const Center(
+                        child: CircularProgressIndicator());
                   }
 
                   return Container(
@@ -138,42 +160,55 @@ class _HomeState extends State<Home> {
                       itemCount: snapshot.data!.docs.length,
                       itemBuilder: (context, index) {
                         final item = snapshot.data!.docs[index];
-                        final data = item.data() as Map<String, dynamic>;
+                        final data =
+                        item.data() as Map<String, dynamic>;
 
-                        return Container(
-                          width: 150,
-                          child: Card(
-                            child: Column(
-                              children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  child: CachedNetworkImage(
-                                    imageUrl: data['img'],
-                                    placeholder: (context, url) =>
-                                    const CircularProgressIndicator(),
-                                    errorWidget: (context, url, error) =>
-                                    const Icon(Icons.error),
-                                    width: double.infinity,
-                                    height: 100,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                ListTile(
-                                  title: Text(
-                                    data['Title'],
-                                    style: GoogleFonts.openSans(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    BlogPage(blogId: item.id),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            width: 150,
+                            child: Card(
+                              child: Column(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius:
+                                    BorderRadius.circular(8.0),
+                                    child: CachedNetworkImage(
+                                      imageUrl: data['img'],
+                                      placeholder: (context, url) =>
+                                      const CircularProgressIndicator(),
+                                      errorWidget: (context, url, error) =>
+                                      const Icon(Icons.error),
+                                      width: double.infinity,
+                                      height: 100,
+                                      fit: BoxFit.cover,
                                     ),
                                   ),
-                                  subtitle: Text(
-                                    'By: ${data['AuthorId']}',
-                                    style: GoogleFonts.openSans(
-                                      fontSize: 12,
+                                  ListTile(
+                                    title: Text(
+                                      data['Title'],
+                                      style: GoogleFonts.openSans(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      'By: ${data['AuthorId']}',
+                                      style: GoogleFonts.openSans(
+                                        fontSize: 12,
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ),
                         );
@@ -195,13 +230,16 @@ class _HomeState extends State<Home> {
 
               StreamBuilder<QuerySnapshot>(
                 stream: _eventsStream,
-                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
                   if (snapshot.hasError) {
                     return const Text('Something went wrong');
                   }
 
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
+                  if (snapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return const Center(
+                        child: CircularProgressIndicator());
                   }
 
                   return Container(
@@ -211,42 +249,55 @@ class _HomeState extends State<Home> {
                       itemCount: snapshot.data!.docs.length,
                       itemBuilder: (context, index) {
                         final item = snapshot.data!.docs[index];
-                        final data = item.data() as Map<String, dynamic>;
+                        final data =
+                        item.data() as Map<String, dynamic>;
 
-                        return Container(
-                          width: 150,
-                          child: Card(
-                            child: Column(
-                              children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  child: CachedNetworkImage(
-                                    imageUrl: data['coverImage'],
-                                    placeholder: (context, url) =>
-                                    const CircularProgressIndicator(),
-                                    errorWidget: (context, url, error) =>
-                                    const Icon(Icons.error),
-                                    width: double.infinity,
-                                    height: 100,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                ListTile(
-                                  title: Text(
-                                    data['title'],
-                                    style: GoogleFonts.openSans(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    EventPage(id: item.id),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            width: 150,
+                            child: Card(
+                              child: Column(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius:
+                                    BorderRadius.circular(8.0),
+                                    child: CachedNetworkImage(
+                                      imageUrl: data['coverImage'],
+                                      placeholder: (context, url) =>
+                                      const CircularProgressIndicator(),
+                                      errorWidget: (context, url, error) =>
+                                      const Icon(Icons.error),
+                                      width: double.infinity,
+                                      height: 100,
+                                      fit: BoxFit.cover,
                                     ),
                                   ),
-                                  subtitle: Text(
-                                    'Start: ${data['start']} - End: ${data['end']}',
-                                    style: GoogleFonts.openSans(
-                                      fontSize: 12,
+                                  ListTile(
+                                    title: Text(
+                                      data['title'],
+                                      style: GoogleFonts.openSans(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      'Start: ${data['start']} - End: ${data['end']}',
+                                      style: GoogleFonts.openSans(
+                                        fontSize: 12,
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ),
                         );
